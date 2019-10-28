88637aff6de030caabaa37db1a7e370a937e3c0d
yorickov <yorickov@gmail.com>
2019-10-28 21:16:21 +0300

    add station test

---
+++ spec/station_spec.rb
@@ -0,0 +1,33 @@
+require 'station'
+require 'train'
+
+RSpec.describe Station do
+  before(:context) do
+    @station = Station.new('Boston')
+    @train = Train.new('1', 'passenger', 3)
+  end
+
+  describe 'methods' do
+    it 'add train' do
+      @station.add_train(@train)
+      expect(@station.trains).to include(@train)
+    end
+
+    it 'show trains' do
+      expect { @station.show_trains }
+        .to output("1\n")
+        .to_stdout
+    end
+
+    it 'show trains by type' do
+      expect { @station.show_trains_by_type('passenger') }
+        .to output("1\n")
+        .to_stdout
+    end
+
+    it 'send train' do
+      @station.send_train(@train)
+      expect(@station.trains.length).to eq 0
+    end
+  end
+end
