a005568a839e778c5586791c2ffd3a8630a343fc
yorickov <yorickov@gmail.com>
2019-10-30 02:11:56 +0300

    small test refactoring

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -6,29 +6,31 @@ describe Station do
   before(:context) do
     @station = Station.new('Boston')
     @passenger_train = PassengerTrain.new('1')
+    @cargo_train = CargoTrain.new('2')
   end
 
-  describe 'passenger methods' do
-    it 'add train' do
-      @station.add_train(@passenger_train)
-      expect(@station.trains).to include(@passenger_train)
-    end
+  it 'add train' do
+    @station.add_train(@passenger_train)
+    @station.add_train(@cargo_train)
+    expect(@station.trains).to include(@passenger_train, @cargo_train)
+  end
 
-    it 'show trains' do
-      expect { @station.show_trains }
-        .to output("1\n")
-        .to_stdout
-    end
+  it 'show trains' do
+    expect { @station.show_trains }.to output("1\n2\n").to_stdout
+  end
 
-    it 'show trains by type' do
-      expect { @station.show_trains_by_type('passenger') }
-        .to output("1\n")
-        .to_stdout
-    end
+  it 'show passenger trains' do
+    expect { @station.show_trains_by_type('passenger') }
+      .to output("1\n").to_stdout
+  end
 
-    it 'send train' do
-      @station.send_train(@passenger_train)
-      expect(@station.trains.size).to eq(0)
-    end
+  it 'show cargo trains' do
+    expect { @station.show_trains_by_type('cargo') }
+      .to output("2\n").to_stdout
+  end
+
+  it 'send train' do
+    @station.send_train(@cargo_train)
+    expect(@station.trains.size).to eq(1)
   end
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -7,21 +7,27 @@ require 'carriage'
 require 'passenger_carriage'
 require 'cargo_carriage'
 
-describe Train, '#light_methods' do
+describe Train, '#speed_methods' do
+  before(:context) do
+    @cargo_train = CargoTrain.new('1')
+  end
+
+  it 'speed up' do
+    @cargo_train.speed_up
+    expect(@cargo_train.speed).to eq(1)
+  end
+
+  it 'slow down' do
+    @cargo_train.slow_down
+    expect(@cargo_train.speed).to eq(0)
+  end
+end
+
+describe Train, '#carriage_methods' do
   before(:context) do
     @passenger_train = PassengerTrain.new('1')
     @passenger_carriage = PassengerCarriage.new
     @cargo_carriage = CargoCarriage.new
-  end
-
-  it 'speed up' do
-    @passenger_train.speed_up
-    expect(@passenger_train.speed).to eq(1)
-  end
-
-  it 'slow down' do
-    @passenger_train.slow_down
-    expect(@passenger_train.speed).to eq(0)
   end
 
   it 'add carriage' do # edit
@@ -29,7 +35,7 @@ describe Train, '#light_methods' do
     expect(@passenger_train.carriages_count).to eq(1)
   end
 
-  it 'add carriage' do # edit
+  it 'add wrong carriage' do # edit
     expect { @passenger_train.add_carriage(@cargo_carriage) }
       .to output("wrong carriage\n")
       .to_stdout
@@ -41,7 +47,7 @@ describe Train, '#light_methods' do
   end
 end
 
-describe Train, '#hard_methods' do
+describe Train, '#move_methods' do
   before(:context) do
     @cargo_rain = CargoTrain.new('1')
     @first_station = Station.new('Boston')
@@ -59,27 +65,17 @@ describe Train, '#hard_methods' do
     expect(@cargo_rain.current_station.name).to eq('NY')
   end
 
+  it 'to next station' do
+    @cargo_rain.to_next_station
+    expect(@cargo_rain.to_next_station).to be_nil
+  end
+
   it 'to previous station' do
     @cargo_rain.to_previous_station
     expect(@cargo_rain.current_station.name).to eq('Boston')
   end
-end
-
-describe Train, '#wrong hard_methods' do
-  before(:context) do
-    @passenger_train = PassengerTrain.new('1')
-    @first_station = Station.new('Boston')
-    @last_station = Station.new('NY')
-    @route = Route.new(@first_station, @last_station)
-    @passenger_train.add_route(@route)
-  end
 
   it 'to previous station' do
-    expect(@passenger_train.to_previous_station).to be_nil
-  end
-
-  it 'to next station' do
-    @passenger_train.to_next_station
-    expect(@passenger_train.to_next_station).to be_nil
+    expect(@cargo_rain.to_previous_station).to be_nil
   end
 end
