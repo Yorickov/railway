00963b40981af4a00e4d10e03a055c6520dc76d9
yorickov <yorickov@gmail.com>
2019-11-02 03:05:58 +0300

    add train service

---
+++ lib/services/train_service.rb
@@ -0,0 +1,121 @@
+class TrainService < Service
+  TRAIN_TYPES = { 'passenger' => PassengerTrain, 'cargo' => CargoTrain }.freeze
+  CARRIAGE_TYPES = { 'passenger' => PassengerCarriage, 'cargo' => CargoCarriage }.freeze
+
+  attr_reader :train_repo, :route_repo
+
+  def initialize(options)
+    @train_repo = options[:train_repo]
+    @route_repo = options[:route_repo]
+    @entity = options[:entity]
+  end
+
+  def create_train
+    puts 'Enter train number'
+
+    train_id = gets.chomp
+
+    if train_repo.trains_list.include?(train_id)
+      puts 'there is already such a number'
+      create_train
+    else
+      puts 'Choose train type (enter index'
+
+      type_index = input_index(TrainService::TRAIN_TYPES.keys)
+      type = TrainService::TRAIN_TYPES.keys[type_index]
+      train = TrainService::TRAIN_TYPES[type].new(train_id)
+
+      train_repo.save(train)
+      puts 'Enter A if you add route to train'
+
+      choice = gets.chomp.downcase
+      add_route_to_train(train) if choice == 'a'
+    end
+  end
+
+  def add_route_to_train(train = nil)
+    if route_repo.data.empty?
+      puts 'there is no routes'
+    elsif train_repo.data.empty?
+      puts 'there is no trains'
+    else
+      route = find_route
+      train ||= find_train
+
+      train.add_route = route
+      train
+    end
+  end
+
+  def find_route
+    puts 'Choose route by the index'
+
+    str_routes = route_repo.data.map { |r| r.station_list.join(', ') } # TODO
+    route_index = input_index(str_routes)
+
+    route_repo.find_by_index(route_index)
+  end
+
+  def find_train
+    puts 'Choose train by the index'
+
+    str_trains = train_repo.data.map(&:info)
+    train_index = input_index(str_trains)
+
+    train_repo.find_by_index(train_index)
+  end
+
+  def move_train
+    train = find_train
+    move_train_process(train)
+  end
+
+  def move_train_process(train)
+    puts 'enter F to move train forwards, B - backwards or X to exit'
+
+    choice = gets.chomp.downcase
+
+    case choice
+    when 'f'
+      train.to_next_station
+    when 'b'
+      train.to_previous_station
+    when 'x'
+      return
+    else
+      move_train_process(train)
+    end
+    move_train_process(train)
+  end
+
+  def create_carriage
+    puts 'enter carriage type'
+
+    type_index = input_index(TrainService::CARRIAGE_TYPES.keys)
+    type = TrainService::CARRIAGE_TYPES.keys[type_index]
+    TrainService::CARRIAGE_TYPES[type].new
+  end
+
+  def manage_carriages
+    train = find_train
+    manage_carriages_process(train)
+  end
+
+  def manage_carriages_process(train)
+    puts 'enter A to add carriage, D to remove or X to exit'
+
+    choice = gets.chomp.downcase
+
+    case choice
+    when 'a'
+      train.add_carriage(create_carriage)
+    when 'd'
+      train.remove_carriage
+    when 'x'
+      return
+    else
+      manage_carriages_process(train)
+    end
+    manage_carriages_process(train)
+  end
+end
--- main.rb
+++ main.rb
@@ -17,15 +17,19 @@ require_relative 'lib/repos/train_repo'
 require_relative 'lib/services/service.rb'
 require_relative 'lib/services/station_service.rb'
 require_relative 'lib/services/route_service.rb'
+require_relative 'lib/services/train_service.rb'
 
 station_repo = StationRepo.new
 route_repo = RouteRepo.new
+train_repo = TrainRepo.new
 
 station_options = { repo: station_repo, entity: Station }
 route_options = { route_repo: route_repo, station_repo: station_repo, entity: Route }
+train_options = { train_repo: train_repo, route_repo: route_repo, entity: Train }
 
 station_service = StationService.new(station_options)
 route_service = RouteService.new(route_options)
+train_service = TrainService.new(train_options)
 
 station_service.create_station_console
 station_service.create_station_console
@@ -35,123 +39,14 @@ station_service.show_stations
 route_service.create_route
 route_service.show_routes
 
+train_service.create_train
+train_service.add_route_to_train
+train_service.manage_carriages
+train_service.move_train
+
 @stations = {}
 @routes = []
 @trains = {}
-
-@train_types = { 'passenger' => PassengerTrain, 'cargo' => CargoTrain }
-@carriage_types = { 'passenger' => PassengerCarriage, 'cargo' => CargoCarriage }
-
-def create_train(type = nil) #----------ADD
-  puts 'Enter train number'
-
-  train_id = gets.chomp
-
-  if @trains.keys.include?(train_id)
-    puts 'there is already such a number'
-    create_train(type)
-  else
-    return @train_types[type].new(train_id) if type
-
-    puts 'Choose train type (enter index'
-
-    type_index = input_index(@train_types.keys)
-    type = @train_types.keys[type_index]
-    train = @train_types[type].new(train_id)
-
-    @trains[train.id] = train
-    puts 'Enter A if you add route to train'
-
-    choice = gets.chomp.downcase
-    add_route_to_train(train) if choice == 'a'
-  end
-end
-
-def input_index(array) #----------ADD
-  array.each.with_index(1) do |item, index|
-    puts "\t#{index}. #{item}"
-  end
-
-  choice = gets.chomp.to_i - 1
-  choice.between?(0, array.size - 1) ? choice : input_index(array)
-end
-
-def add_route_to_train(train = nil) #----------ADD
-  if @routes.empty?
-    puts 'there is no routes'
-  elsif @trains.empty?
-    puts 'there is no trains'
-  else
-    route = find_route
-    train ||= find_train
-
-    train.add_route = route
-    train
-  end
-end
-
-def find_train #----------ADD
-  puts 'Choose train by the index'
-
-  str_trains = @trains.values.map(&:info)
-  train_index = input_index(str_trains)
-
-  @trains.values[train_index]
-end
-
-def move_train
-  train = find_train
-  move_train_process(train)
-end
-
-def move_train_process(train) #----------ADD
-  puts 'enter F to move train forwards, B - backwards or X to exit'
-
-  choice = gets.chomp.downcase
-
-  case choice
-  when 'f'
-    train.to_next_station
-  when 'b'
-    train.to_previous_station
-  when 'x'
-    return
-  else
-    move_train_process(train)
-  end
-  move_train_process(train)
-end
-
-def create_carriage #-------------------ADD
-  puts 'enter carriage type'
-
-  type_index = input_index(@carriage_types.keys)
-  type = @carriage_types.keys[type_index]
-  @carriage_types[type].new
-end
-
-def manage_carriages
-  train = find_train
-  manage_carriages_process(train)
-end
-
-def manage_carriages_process(train) #-------------------ADD
-  puts 'enter A to add carriage, D to remove or X to exit'
-
-  choice = gets.chomp.downcase
-
-  case choice
-  when 'a'
-    train.add_carriage(create_carriage)
-  when 'd'
-    train.remove_carriage
-  when 'x'
-    return
-  else
-    manage_carriages_process(train)
-  end
-  manage_carriages_process(train)
-end
 
 # def station_management
 #   puts "Enter C to create station,\n" \
